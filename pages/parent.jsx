d716a5ca3e98d2c2b0b548d19826eeac5d73dbb5
kv8gh <vyaskaran1409@gmail.com>
2024-02-23 03:46:14 +0530

    submit logic

---
+++ pages/parent.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+
+export default function Parent() {
+    const [isOpen,setIsOpen] = useState(false);
+    return (
+        <main>
+            {/* <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <span className="sr-only">Open sidebar</span>
+            </button> */}
+            
+            
+            
+        </main>
+    );
+}
